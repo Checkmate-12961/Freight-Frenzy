e0101bab77b4f3a3d6e88255dea47f7930ddd696
Andrew Craig <andrewdavidcraig@gmail.com>
2021-09-25 12:53:43 -0400

    added a temporary transformation for the camera position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RealsenseLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RealsenseLocalizer.java
@@ -7,13 +7,15 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.localization.Localizer;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Transform2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.spartronics4915.lib.T265Camera;
 
 import org.firstinspires.ftc.teamcode.robot.util.Pose2dConversionUtil;
 
 public class RealsenseLocalizer implements Localizer {
-    public static Transform2d cameraRobotOffset = new Transform2d();
+    // TODO: tune this based on the relative positions of the camera and the robot
+    public static Transform2d cameraRobotOffset = new Transform2d(new Translation2d(9.0,8.25), new Rotation2d());
     public double encoderMeasurementCovariance = 0.8;
 
     private Pose2d lastPose = null;
