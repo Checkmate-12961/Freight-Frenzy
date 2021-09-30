4d39b2a8499c2e45fa1b6525f9740acb73880068
ryleu <riley.j.mclain@gmail.com>
2021-09-30 18:15:36 -0400

    moved TuningMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/TuningMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/TuningMode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.robot;
+package org.firstinspires.ftc.teamcode.opmodes.tuning;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
