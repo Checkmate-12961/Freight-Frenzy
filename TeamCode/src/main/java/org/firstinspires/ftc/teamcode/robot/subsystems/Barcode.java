eba78b267f740fbce7e3a57cbf8cf268974e2735
Andrew Craig <andrewdavidcraig@gmail.com>
2021-10-18 14:25:07 -0400

    Added barcode functionality. Did not implement. It should work, but it there is work to do improving it.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Barcode.java
@@ -0,0 +1,158 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import org.firstinspires.ftc.teamcode.robot.subsystems.drivetrain.BarcodeConstants;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+
+public class Barcode {
+    public enum BarcodePosition {LEFT, MIDDLE, RIGHT}
+
+    private final RingDeterminationPipeline pipeline = new RingDeterminationPipeline();
+
+
+    private static class RingDeterminationPipeline extends OpenCvPipeline {
+        //Some color constants
+        //These dictate the color of the boxes when you see the camera output
+        private static final Scalar RED = new Scalar(255, 0, 0);
+        private static final Scalar YELLOW = new Scalar(255, 255, 0);
+        private static final Scalar BLUE = new Scalar(0, 0, 255);
+        private static final Scalar PURPLE = new Scalar(51, 12, 47);
+
+        //The core values which define the location and size of the sample regions
+        private final Point LEFTBOX_TOPLEFT_ANCHOR_POINT = new Point(BarcodeConstants.LeftBoxX, BarcodeConstants.LeftBoxY);
+
+        private final Point LEFTBOX_pointA = new Point(
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.x,
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.y);
+        private final Point LEFTBOX_pointB = new Point(
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.x + BarcodeConstants.LeftBoxWidth,
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.y + BarcodeConstants.LeftBoxHeight);
+
+        private final Point MIDDLEBOX_TOPLEFT_ANCHOR_POINT = new Point(BarcodeConstants.MiddleBoxX, BarcodeConstants.MiddleBoxY);
+
+        private final Point MIDDLEBOX_pointA = new Point(
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.x,
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.y);
+        private final Point MIDDLEBOX_pointB = new Point(
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.x + BarcodeConstants.MiddleBoxWidth,
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.y + BarcodeConstants.MiddleBoxHeight);
+
+        private final Point RIGHTBOX_TOPLEFT_ANCHOR_POINT = new Point(BarcodeConstants.RightBoxX, BarcodeConstants.RightBoxY);
+
+        private final Point RIGHTBOX_pointA = new Point(
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.x,
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.y);
+        private final Point RIGHTBOX_pointB = new Point(
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.x + BarcodeConstants.RightBoxWidth,
+                LEFTBOX_TOPLEFT_ANCHOR_POINT.y + BarcodeConstants.RightBoxHeight);
+        /*
+         * Working variables
+         */
+        private Mat LEFTBOX_Cb;
+        private Mat MIDDLEBOX_Cb;
+        private Mat RIGHTBOX_Cb;
+        private final Mat YCrCb = new Mat();
+        private final Mat Cb = new Mat();
+        private int leftValue;
+        private int middleValue;
+        private int rightValue;
+
+        // Volatile since accessed by OpMode thread w/o synchronization
+        private volatile BarcodePosition position = BarcodePosition.LEFT;
+
+        /*
+         * This function takes the RGB frame, converts to YCrCb,
+         * and extracts the Cb channel to the 'Cb' variable
+         */
+        private void inputToCb(Mat input) {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(YCrCb, Cb, 1);
+        }
+
+        @Override
+        public void init(Mat firstFrame) {
+            inputToCb(firstFrame);
+
+            LEFTBOX_Cb = Cb.submat(new Rect(LEFTBOX_pointA, LEFTBOX_pointB));
+            MIDDLEBOX_Cb = Cb.submat(new Rect(MIDDLEBOX_pointA, MIDDLEBOX_pointB));
+            RIGHTBOX_Cb = Cb.submat(new Rect(RIGHTBOX_pointA, RIGHTBOX_pointB));
+        }
+
+        @Override
+        public Mat processFrame(Mat input) {
+            inputToCb(input);
+
+            leftValue = (int) Core.mean(LEFTBOX_Cb).val[0];
+            middleValue = (int) Core.mean(MIDDLEBOX_Cb).val[0];
+            rightValue = (int) Core.mean(RIGHTBOX_Cb).val[0];
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    LEFTBOX_pointA, // First point which defines the rectangle
+                    LEFTBOX_pointB, // Second point which defines the rectangle
+                    PURPLE, // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    MIDDLEBOX_pointA, // First point which defines the rectangle
+                    MIDDLEBOX_pointB, // Second point which defines the rectangle
+                    PURPLE, // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    LEFTBOX_pointA, // First point which defines the rectangle
+                    LEFTBOX_pointB, // Second point which defines the rectangle
+                    PURPLE, // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
+
+
+            position = BarcodePosition.LEFT; // Record our analysis
+            if ((leftValue>middleValue) && (leftValue>rightValue)) {
+                position = BarcodePosition.LEFT;
+            } else if (middleValue>rightValue) {
+                position = BarcodePosition.MIDDLE;
+            } else {
+                position = BarcodePosition.RIGHT;
+            }
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    LEFTBOX_pointA, // First point which defines the rectangle
+                    LEFTBOX_pointB, // Second point which defines the rectangle
+                    RED, // The color the rectangle is drawn in
+                    -1); // Negative thickness means solid fill
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    MIDDLEBOX_pointA, // First point which defines the rectangle
+                    MIDDLEBOX_pointB, // Second point which defines the rectangle
+                    YELLOW, // The color the rectangle is drawn in
+                    -1); // Negative thickness means solid fill
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    RIGHTBOX_pointA, // First point which defines the rectangle
+                    RIGHTBOX_pointB, // Second point which defines the rectangle
+                    BLUE, // The color the rectangle is drawn in
+                    -1); // Negative thickness means solid fill
+
+            return input;
+        }
+
+        public int getAnalysis() {
+            return leftValue;
+        }
+
+        public BarcodePosition getPosition() {
+            return position;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/drivetrain/BarcodeConstants.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems.drivetrain;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class BarcodeConstants {
+    public static int LeftBoxX = 0;
+    public static int LeftBoxY = 0;
+    public static int LeftBoxWidth = 0;
+    public static int LeftBoxHeight = 0;
+
+    public static int MiddleBoxX = 0;
+    public static int MiddleBoxY = 0;
+    public static int MiddleBoxWidth = 0;
+    public static int MiddleBoxHeight = 0;
+
+    public static int RightBoxX = 0;
+    public static int RightBoxY = 0;
+    public static int RightBoxWidth = 0;
+    public static int RightBoxHeight = 0;
+}
