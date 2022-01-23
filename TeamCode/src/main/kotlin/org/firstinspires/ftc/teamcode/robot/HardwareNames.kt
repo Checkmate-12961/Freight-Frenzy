0d6ece3c9534b77327e892978d3f82b3c7aa2667
ryleu <riley.j.mclain@gmail.com>
2022-01-23 16:02:12 -0500

    Fix lift companion motion... take 3

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/HardwareNames.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/HardwareNames.kt
@@ -46,7 +46,7 @@ class HardwareNames {
         // Carousel mechanism
         CAROUSEL("carousel", false),
         // Intake mechanism
-        INTAKE("intake", true),
+        INTAKE("intake", false),
         // Lift mechanism
         LIFT("lift", false);
 
